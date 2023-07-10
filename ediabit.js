ddc4c833dd8fe527fe626c3bf469371fef01f8a2
Mustafa <MustafaGino1@yahoo.com>
2023-07-09 23:55:35 -0500

    problem #3.

--- ediabit.js
+++ ediabit.js
@@ -38,3 +38,14 @@ function towerHanoi(discs) {
 // REGEXP.test("") ➞ true
 const REGEXP = /^$/;
 console.log(REGEXP.test(""));
+
+
+// 3) Odd numbers like to hangout with odd numbers. Even numbers like to hangout with even numbers.
+
+// A spot is an insertion between two numbers in an array. Given an array of n integers in length, there are n-1 spots available.
+
+// For instance:
+
+// [3, 4, 9, 10, 1]  // Array of 5 digits can also be thought of as...
+
+// [3, __ , 4, __ , 9, __, 10, __, 1]  // ...an array of 4 spots.
