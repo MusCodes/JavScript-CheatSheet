6d3fa95aaeb74af167b5a2b508d8cdeea6facb85
Mustafa <MustafaGino1@yahoo.com>
2023-07-09 23:55:58 -0500

    formatted for better readablility.

--- ediabit.js
+++ ediabit.js
@@ -14,18 +14,14 @@
 // The amount of discs is always a positive integer.
 // 1 disc can be changed per move.
 function towerHanoi(discs) {
-    if (discs === 0) {
-      return 0;
-    } else if (discs === 1) {
-      return 1;
-    } else {
-      return 2 * towerHanoi(discs - 1) + 1;
-    }
+  if (discs === 0) {
+    return 0;
+  } else if (discs === 1) {
+    return 1;
+  } else {
+    return 2 * towerHanoi(discs - 1) + 1;
   }
-
-
-
-
+}
 
 // 2) If you've completed this RegEx series from I to XXII then you have been exposed to all of MDN's documentation on regular expressions special characters. You can check my Collections under Basic Reg Ex in my profile if you missed any. This next part of the series is to help solidify what you've learned. In order to save time I will be searching the web to find regular expression exercises to post here.
 
@@ -38,7 +34,6 @@ function towerHanoi(discs) {
 // REGEXP.test("") ➞ true
 const REGEXP = /^$/;
 console.log(REGEXP.test(""));
-
 
 // 3) Odd numbers like to hangout with odd numbers. Even numbers like to hangout with even numbers.
 
