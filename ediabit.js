ede3947307ef7122f91b38474f764fcc8c1b4e3d
Mustafa <MustafaGino1@yahoo.com>
2023-07-09 23:58:40 -0500

    problem #5.

--- ediabit.js
+++ ediabit.js
@@ -63,3 +63,13 @@ console.log(REGEXP.test(""));
 // changeEnough([10, 0, 0, 50], 3.85) ➞ false
 
 // changeEnough([1, 0, 5, 219], 19.99) ➞ false
+
+
+// 5) Create a function that takes an array of numbers and returns the second largest number.
+
+// Examples
+// secondLargest([10, 40, 30, 20, 50]) ➞ 40
+
+// secondLargest([25, 143, 89, 13, 105]) ➞ 105
+
+// secondLargest([54, 23, 11, 17, 10]) ➞ 23
