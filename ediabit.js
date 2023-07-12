8156eff33e9df36d000f18f6d9266919e12fa66f
Mustafa <MustafaGino1@yahoo.com>
2023-07-11 23:49:35 -0500

    question number 8.

--- ediabit.js
+++ ediabit.js
@@ -128,6 +128,7 @@ console.log(arrangedNumbers);
 
 
 
+
 //  7)  You will be given an array of drinks, with each drink being an object with two properties: name and price. Create a function that has the drinks array as an argument and return the drinks objects sorted by price in ascending order.
 
 // Assume that the following array of drink objects needs to be sorted:
@@ -140,3 +141,18 @@ console.log(arrangedNumbers);
 
 // Examples
 // sortDrinkByPrice(drinks) ➞ [{name: "lime", price: 10}, {name: "lemonade", price: 50}]
+
+
+
+
+
+
+//  8)  Create a function that takes an array of arrays with numbers. Return a new (single) array with the largest numbers of each.
+
+// Examples
+// findLargestNums([[4, 2, 7, 1], [20, 70, 40, 90], [1, 2, 0]]) ➞ [7, 90, 2]
+
+// findLargestNums([[-34, -54, -74], [-32, -2, -65], [-54, 7, -43]]) ➞ [-34, -2, 7]
+
+// findLargestNums([[0.4321, 0.7634, 0.652], [1.324, 9.32, 2.5423, 6.4314], [9, 3, 6, 3]]) ➞ [0.7634, 9.32, 9]
+
